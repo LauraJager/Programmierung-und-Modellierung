71566ba7c4abafd87b087f5f6273797f071b48c6
LauraJager <74593291+LauraJager@users.noreply.github.com>
2020-11-17 10:34:01 +0100

    Add files via upload

---
+++ Abgabe1 - basics.java
@@ -0,0 +1,30 @@
+
+public class Functionality {
+
+	public static double trapezoidArea(int a, int c, int h) {
+		double trapezFlaeche = ((double) a + (double) c) * h * 0.5;
+		if (((a <= 0) || (c <= 0)) || (h <= 0)) {
+			System.out.println(-1.0);
+			return -1.0;
+		} else {
+			System.out.println(trapezFlaeche);
+			return trapezFlaeche;
+		}
+
+	}
+
+	public static double createDouble(int a, int b) {
+		while ((b % 10) == 0) {
+			b = b / 10;
+		}
+		String a1 = Integer.toString(a);
+		String b1 = Integer.toString(b);
+		String ausgabeString = a1 + "." + b1;
+		double ausgabeDouble = Double.parseDouble(ausgabeString);
+		return ausgabeDouble;
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
